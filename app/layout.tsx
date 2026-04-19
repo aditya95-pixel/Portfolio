6004cae53908c60563f986ea98d8cb46bd4b0b3d
aditya95-pixel <sphs.am6@gmail.com>
2026-04-19 07:51:46 +0530

    metadata cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://portfolio-beta-snowy-3wzw0loxty.vercel.app'),
   title: "Aditya Mazumdar's Portfolio",
   description: "A website showcasing my coding exploits over college life",
+  openGraph: {
+    images: [
+      {
+        url: '/assets/portfolio.png', // Looks in the public folder
+        width: 1200,
+        height: 630,
+        alt: "Aditya Mazumdar's Portfolio Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
